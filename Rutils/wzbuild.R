33849fb55d74d49eca64f8384b5273809ee4babb
zwdzwd <zhouwanding@gmail.com>
2016-04-27 18:41:07 -0400

    minor, message omitted

--- Rutils/wzbuild.R
+++ Rutils/wzbuild.R
@@ -1,4 +1,21 @@
 #!/usr/bin/env Rscript
-library(devtools)
 
-document(commandArgs(trailingOnly=TRUE))
+target <- commandArgs(trailingOnly=TRUE)
+format <- "html"
+if (length(target) > 1) {
+  format <- target[2]
+  target <- target[1]
+}
+
+if (dir.exists(target)) {
+  library(devtools)
+  document()
+} else {
+  if (format == 'pdf') {
+    library(rmarkdown)
+    render(target, output_format='pdf_document')
+  } else {
+    library(rmarkdown)
+    render(target)
+  }
+}
