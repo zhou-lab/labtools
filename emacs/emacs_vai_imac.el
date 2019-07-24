efebc21c72fddbfdc2c44811cd6fe7963e18485f
Wanding Zhou <wanding.zhou@vai.org>
2019-07-24 19:07:57 -0400

    minor, message omitted

--- emacs/emacs_vai_imac.el
+++ emacs/emacs_vai_imac.el
@@ -24,7 +24,7 @@
 ;; (add-to-list 'default-frame-alist '(font . "Monaco-12"))
 ;; (add-to-list 'default-frame-alist '(font . "Iosevka-12:light"))
 ;; (add-to-list 'default-frame-alist '(font . "Anonymous Pro-13"))
-(add-to-list 'default-frame-alist '(font . "Anonymous Pro-16"))
+;; (add-to-list 'default-frame-alist '(font . "Anonymous Pro-16"))
 
 ;; (require 'ess)
 (load "ess-site")
