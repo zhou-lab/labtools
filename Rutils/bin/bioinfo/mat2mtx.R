62f314591387f3a1ab4bf2cbfe8f68ba25bd69c2
Wanding Zhou <zhouw3@email.chop.edu>
2020-01-02 17:35:07 -0500

    removed pbsgen, see that in PipelineCollections

--- Rutils/bin/bioinfo/mat2mtx.R
+++ Rutils/bin/bioinfo/mat2mtx.R
@@ -4,4 +4,4 @@ stopifnot(length(args) >= 2)
 library(rmatio)
 library(Matrix) ## sparse matrix dgCMatrix
 m <- read.mat(args[[1]])
-writeMM(m, file=paste(args[[2]]))
+saveRDS(m, file=paste(args[[2]]))
