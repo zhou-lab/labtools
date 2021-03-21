3c6e6dff69b72eec4be285f9a036cc0e6e289894
Wanding Zhou <zhouw3@email.chop.edu>
2021-03-21 10:49:19 -0400

    minor, message omitted

--- Rutils/bin/basics/excel2tsv.R
+++ Rutils/bin/basics/excel2tsv.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
-library(tidyverse)
-library(readxl)
+suppressMessages(library(tidyverse))
+suppressMessages(library(readxl))
 args <- commandArgs(trailingOnly=TRUE)
 
 # '/Users/zhouw3/repo/samplesheets/201x/2016_04_05_TCGA_merged_mapping.xlsx'
