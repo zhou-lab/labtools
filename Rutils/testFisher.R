885a854794bd8e20daf504fa4d7e6a99fac7c699
Wanding Zhou <zhouwanding@gmail.com>
2022-10-05 11:05:11 -0400

    minor, message omitted

--- Rutils/testFisher.R
+++ Rutils/testFisher.R
@@ -6,7 +6,7 @@ df = read.table(args[1], header=T) %>% dplyr::mutate(
     nf=nfmq+nfq,
     nqmf=nq-nfq,
     numfq=nu-nf-nq+nfq,
-    p_val_dep = phyper(nfq, nf, nu-nf, nq, lower.tail=TRUE),
-    p_val_enr = phyper(nfq-1, nf, nu-nf, nq, lower.tail=TRUE),
+    p_val_enr = phyper(nfq, nf, nu-nf, nq, lower.tail=TRUE),
+    p_val_dep = phyper(nfq-1, nf, nu-nf, nq, lower.tail=FALSE),
     odds_ratio=(nfq/nfmq)/(nqmf/numfq)) %>% dplyr::arrange(p_val_enr)
 write.table(format(df), file=args[2], quote=FALSE, row.names=FALSE, sep="\t")
