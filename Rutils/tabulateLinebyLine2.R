0d69a00cbaa1613100537b01ffe7409f6e0485c9
Wanding Zhou <zhouw3@email.chop.edu>
2022-10-04 20:49:02 -0400

    minor, message omitted

---
+++ Rutils/tabulateLinebyLine2.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+library(stringr)
+library(parallel)
+
+con = file("stdin", "r")
+while(length(lines <- readLines(con, n = 100000)) > 0) { # 100k is best
+    cat(paste0(mclapply(lines, function(line) {
+        a = sort(table(str_split(line, "\t")), decreasing=TRUE)
+        sprintf("%s\t%s", names(a)[names(a)!="NA"][1],
+            paste0(sprintf("%s;%d", names(a), a), collapse=","))
+    }, mc.cores=24), collapse="\n"),"\n", sep="")
+}
