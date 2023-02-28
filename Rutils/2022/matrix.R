b3d154a1c753ef249006e6bc3b7cb0487b02f98a
Wanding Zhou <zhouwanding@gmail.com>
2023-02-28 17:45:00 -0500

    minor, message omitted

--- Rutils/2022/matrix.R
+++ Rutils/2022/matrix.R
@@ -3,8 +3,8 @@ cleanMatrixForClusterW <- function(mtx, f_row = 0.5, f_col = 0.5) {
         f_row, f_col))
     cat("Before: ", nrow(mtx), "rows and ", ncol(mtx),"columns.\n")
     namtx = is.na(mtx)
-    good_row = rowSums(namtx) <= ncol(mtx) * (1-f_row)
-    good_col = colSums(namtx) <= nrow(mtx) * (1-f_col)
+    good_row = rowSums(namtx) <= ncol(mtx) * f_row
+    good_col = colSums(namtx) <= nrow(mtx) * f_col
     cat("After: ", sum(good_row), "rows and ", sum(good_col),"columns.\n")
     mtx[good_row, good_col]
 }
