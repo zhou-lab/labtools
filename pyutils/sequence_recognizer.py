dccff5e569264bfdc3d77860a0474b71f31b08e3
Wanding Zhou <zhouw3@email.chop.edu>
2023-11-09 11:49:18 -0500

    update to seqeucne recognizer

--- pyutils/sequence_recognizer.py
+++ pyutils/sequence_recognizer.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 import argparse
 from fuzzysearch import find_near_matches
+from sequence_operations_common import reverse_complement
 
 def check_one_read_standalone(read, seq, max_len):
     return find_near_matches(seq, read, max_l_dist=max_len)
 
-# python3 -c 'import SHAREseq_tools; SHAREseq_tools.true_false_table("SEPA", "seqs", "nums", "1.csv")'
-# python3 -c 'import SHAREseq_tools; SHAREseq_tools.table_analysis("1.csv",[1], 0, 100000000000)'
-# python3 -c 'import SHAREseq_tools; SHAREseq_tools.check_one_read_standalone("ATGCATGCATGC", "ATGC", 0)'
+def sort_key_dist(obj):
+    return obj.dist
 
 parser = argparse.ArgumentParser(description="This is a command line tool to analyze your SHARE-seq data or other data that includes barcodes.")
 group = parser.add_mutually_exclusive_group()
+group.add_argument("-U", "--ultimate_checking", action="store_true", help="Returns true if it is a good read for a split seq based sequencing")
 group.add_argument("-c", "--contains", action="store_true", help="Very simple tool, returns T/F for whether a read contains a sequence.")
-group.add_argument("-t", "--table", action="store_true", help="Prints a table with information of the matches.")
+group.add_argument("-t", "--table", action="store_true", help="Prints a table with information of the matches. The columns are: start, end, edit distance, matched sequence in the read.")
 group.add_argument("-a", "--annotate", action="store_true", help="Annotates the finding on the sequence.")
+parser.add_argument("-f", "--flip", action="store_true", help="If you include this, all the reverse complementary sequences of the sequences you enter will also be computed.")
 parser.add_argument("-r", "--read", type=str, help="The enter read.")
 parser.add_argument("-s", "--sequences", type=str, nargs='+', help="The sequences that should be contained.")
 parser.add_argument("-m", "--max_distances", type=int, default=2, nargs='+', help="The maximum edit distance.")
@@ -23,12 +25,74 @@ if len(args.sequences) != len(args.max_distances):
     print("The sequences and the maximum distances do not match!")
     exit()
 
-for (seq, max_len) in zip(args.sequences, args.max_distances):
+sequences = [element for element in args.sequences]
+max_distances = [element for element in args.max_distances]
+
+if args.ultimate_checking:
+    contain_all = True
+    contain_all_flip = True
+    for (seq, max_len) in zip(sequences, max_distances):
+        if not len(check_one_read_standalone(args.read, seq, max_len)) > 0:
+            contain_all = False
+        if not len(check_one_read_standalone(args.read, reverse_complement(seq), max_len)) > 0:
+            contain_all_flip = False
+    if contain_all:
+        forward = []
+        forward_check = True
+        for (seq, max_len) in zip(sequences, max_distances):
+            forward.append(sorted(check_one_read_standalone(args.read, seq, max_len), key=sort_key_dist)[0])
+        for i in range(len(forward)):
+            if i < len(forward)-1:
+                if forward[i].end > forward[i+1].start:
+                    forward_check = False
+                    break
+        if forward_check:
+            print("F", end="\t")
+            for i in range(len(forward)):
+                # if i < len(forward)-1:
+                #     print(args.read[forward[i].end:forward[i+1].start], end="\t")
+                print(str(forward[i].start) + "\t" + str(forward[i].end) + "\t" + str(forward[i].dist) + "\t" + forward[i].matched, end="\t")
+        else:
+            print("False", end="\t")
+    elif contain_all_flip:
+        backward = []
+        backward_check = True
+        for (seq, max_len) in zip(sequences, max_distances):
+            backward.append(sorted(check_one_read_standalone(args.read, reverse_complement(seq), max_len), key=sort_key_dist)[0])
+        for i in range(len(backward)):
+            if i < len(backward)-1:
+                if backward[i].start < backward[i+1].end:
+                    backward_check = False
+                    break
+        if backward_check:
+            print("B", end="\t")
+            for i in range(len(backward)):
+                # if i < len(backward)-1:
+                #     print(args.read[backward[i+1].end:backward[i].start], end="\t")
+                print(str(backward[i].start) + "\t" + str(backward[i].end) + "\t" + str(backward[i].dist) + "\t" + backward[i].matched, end="\t")
+        else:
+            print("False", end="\t")
+    else:
+        print("False", end="\t")
+    print()
+    exit()
+
+if args.flip:
+    for (seq, max_len) in zip(args.sequences, args.max_distances):
+        sequences.append(reverse_complement(seq))
+        max_distances.append(max_len)
+
+
+for (seq, max_len) in zip(sequences, max_distances):
     if args.contains:
         print(str(len(check_one_read_standalone(args.read, seq, max_len)) > 0), end="\t")
     elif args.table:
-        print("Sequence: " + seq)
-        print(check_one_read_standalone(args.read, seq, max_len))
+        #print("Sequence: " + seq)
+        if len(check_one_read_standalone(args.read, seq, max_len)) > 0:
+            matched_dic = check_one_read_standalone(args.read, seq, max_len)[0]
+            print("Seq:" + seq + "\t"  + str(matched_dic.start) + "\t" + str(matched_dic.end) + "\t" + str(matched_dic.dist) + "\t" + str(matched_dic.matched), end="\t")
+        #else:
+        #    print("-1\t-1\t-1\tX")
     elif args.annotate:
         print("Sequence: " + seq)
         for match in check_one_read_standalone(args.read, seq, max_len):
@@ -37,6 +101,7 @@ for (seq, max_len) in zip(args.sequences, args.max_distances):
             print("Match:", end=" ")
             print(args.read[:match.start] + CRED + args.read[match.start:match.end] + CEND + args.read[match.end:])
 
+print() ## This is to give a new line.
 
 # Example runs:
 # python3 sequence_recognizer.py -c -r ACTTCCGTTCAGTTACGTATTGCTAGATGTGTATAAGAGACAGAGGATAAATCATGCTAAGGCGAGGATGAAGCCGATATCGCCGATACGGTTGTATAGGATTGCTTGAATGGCTGCTGTGTTGGCATCTGCTCGGGCGTATCATCAACTGATGAGCAAAAAGAAGGATGTAATTCCGTT -s CTGTCTCTTATACAC GTGTATAAGAGACAG -m 2 2
--- pyutils/targeted_trimadapters.py
+++ pyutils/targeted_trimadapters.py
@@ -13,17 +13,6 @@ def read_fastq(file):
     fos = list(fos)
     return fos
 
-# def read_fastq_folder(folder):
-#     foss = []
-#     for filename in os.listdir(folder):
-#         if not filename.startswith('.') and filename.endswith('fastq'):
-#             f = os.path.join(folder, filename)
-#             if os.path.isfile(f):
-#                 fos = read_fastq(f)
-#                 for fo in fos:
-#                     foss.append(fo)
-#     return foss
-
 def read_fastq_file(folder, filename):
     foss = []
     if not filename.startswith('.') and filename.endswith('fastq'):
@@ -34,50 +23,47 @@ def read_fastq_file(folder, filename):
                 foss.append(fo)
     return foss
 
-def trimming(fastq_folder, seqs, max_lens, output):
+def trimming(fos, seqs, max_lens, output):
 
-    for filename in os.listdir(fastq_folder):
+    for fastq_obj in fos:
+        fos_trimmed = []
+        length = len(fastq_obj.getSeq())
+        start_coors = -1
+        end_coors = math.inf
 
-        fos = read_fastq_file(fastq_folder, filename)
-
-        for fastq_obj in fos:
-            fos_trimmed = []
-            length = len(fastq_obj.getSeq())
-            start_coors = -1
-            end_coors = math.inf
-
-            for (seq, max_len) in zip(seqs, max_lens):
-                ad_matches = find_near_matches(seq, fastq_obj.getSeq(), max_l_dist=max_len)
-                if len(ad_matches) > 0:
-                    if ad_matches[0].end - 0 > length - ad_matches[0].start:
-                        if ad_matches[0].start < end_coors:
-                            end_coors = ad_matches[0].start
-                    else:
-                        if ad_matches[0].end > start_coors:
-                            start_coors = ad_matches[0].end
+        for (seq, max_len) in zip(seqs, max_lens):
+            ad_matches = find_near_matches(seq, fastq_obj.getSeq(), max_l_dist=max_len)
+            if len(ad_matches) > 0:
+                if ad_matches[0].end - 0 > length - ad_matches[0].start:
+                    if ad_matches[0].start < end_coors:
+                        end_coors = ad_matches[0].start
+                else:
+                    if ad_matches[0].end > start_coors:
+                        start_coors = ad_matches[0].end
     
-            if start_coors == -1 and end_coors == math.inf:
-                fos_trimmed.append(fastq_obj)
-            elif start_coors == -1:
-                head_trimmed = fastq_obj.getHead()[:37]
-                seq_trimmed = fastq_obj.getSeq()[:end_coors]
-                qual_trimmed = fastq_obj.getQual()[:end_coors]
-                fos_trimmed.append(fq.fastq_object(head_trimmed, seq_trimmed, qual_trimmed))
-            elif end_coors == math.inf:
-                head_trimmed = fastq_obj.getHead()[:37]
-                seq_trimmed = fastq_obj.getSeq()[start_coors:]
-                qual_trimmed = fastq_obj.getQual()[start_coors:]
-                fos_trimmed.append(fq.fastq_object(head_trimmed, seq_trimmed, qual_trimmed))
-            elif end_coors - start_coors > 10:
-                head_trimmed = fastq_obj.getHead()[:37]
-                seq_trimmed = fastq_obj.getSeq()[start_coors:end_coors]
-                qual_trimmed = fastq_obj.getQual()[start_coors:end_coors]
-                fos_trimmed.append(fq.fastq_object(head_trimmed, seq_trimmed, qual_trimmed))
-            write_fastq(fos_trimmed, output)
+        if start_coors == -1 and end_coors == math.inf:
+            fos_trimmed.append(fastq_obj)
+        elif start_coors == -1:
+            head_trimmed = fastq_obj.getHead()[:37]
+            seq_trimmed = fastq_obj.getSeq()[:end_coors]
+            qual_trimmed = fastq_obj.getQual()[:end_coors]
+            fos_trimmed.append(fq.fastq_object(head_trimmed, seq_trimmed, qual_trimmed))
+        elif end_coors == math.inf:
+            head_trimmed = fastq_obj.getHead()[:37]
+            seq_trimmed = fastq_obj.getSeq()[start_coors:]
+            qual_trimmed = fastq_obj.getQual()[start_coors:]
+            fos_trimmed.append(fq.fastq_object(head_trimmed, seq_trimmed, qual_trimmed))
+        elif end_coors - start_coors > 10:
+            head_trimmed = fastq_obj.getHead()[:37]
+            seq_trimmed = fastq_obj.getSeq()[start_coors:end_coors]
+            qual_trimmed = fastq_obj.getQual()[start_coors:end_coors]
+            fos_trimmed.append(fq.fastq_object(head_trimmed, seq_trimmed, qual_trimmed))
+        write_fastq(fos_trimmed, output)
 
-parser = argparse.ArgumentParser(description="This is a command line tool trim off any adapter/barcode/sequence you don't want. An example of running this tool: python3 targeted_trimadapters.py -f /path/to/directory -s TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG CTGTCTCTTATACACATCTGACGCTGCCGACGA GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG CTGTCTCTTATACACATCTCCGAGCCCACGAGAC -m 3 3 3 3 -o /path/to/directory/name.fastq")
-# group = parser.add_mutually_exclusive_group()
-parser.add_argument("-f", "--folder", type=str, help="The folder that includes all fastq files.")
+parser = argparse.ArgumentParser(description="This is a command line tool trim off any adapter/barcode/sequence you don't want. An example of running this tool: python3 targeted_trimadapters.py -d /path/to/directory -s TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG CTGTCTCTTATACACATCTGACGCTGCCGACGA GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG CTGTCTCTTATACACATCTCCGAGCCCACGAGAC -m 3 3 3 3 -o /path/to/directory/name.fastq")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-d", "--directory", type=str, help="The folder that includes all fastq files.")
+group.add_argument("-f", "--file", type=str, help="The file that is a fastq.")
 parser.add_argument("-s", "--sequences", type=str, nargs='+', help="The sequences that should be contained.")
 parser.add_argument("-m", "--max_distances", type=int, nargs='+', help="The maximum edit distance.")
 parser.add_argument("-o", "--output", type=str, help="The file for storing trimmed reads.")
@@ -88,7 +74,13 @@ if len(args.sequences) != len(args.max_distances):
     print(str(len(args.sequences)) + " does not equal to " + str(len(args.max_distances)))
     exit()
 
-trimming(args.folder, args.sequences, args.max_distances, args.output)
+if args.directory:
+    for filename in os.listdir(fastq_folder):
+        fos = read_fastq_file(fastq_folder, filename)
+        trimming(fos, args.sequences, args.max_distances, args.output)
+else:
+    fos = read_fastq(args.file)
+    trimming(fos, args.sequences, args.max_distances, args.output)
 
     
 
