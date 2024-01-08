6abe3370a77e7a3a276693c2360ccffd0dc1c22d
Wanding Zhou <zhouwanding@gmail.com>
2024-01-08 10:04:27 -0500

    minor, message omitted

--- Rutils/2022/signature_selection.R
+++ Rutils/2022/signature_selection.R
@@ -283,7 +283,8 @@ rowClusterTissueSE <- function(se) {
     se[rownames(betas2),]
 }
 
-pack_cm_file <- function(se_sig, ref="mm10", tmpdir="~/tmp/") {
+## pack SummarizedExperiment of .rds (from WGBS) into a .cx feature file
+pack_cx_file <- function(se_sig, ref="mm10", tmpdir="~/tmp/") {
     temp_file <- tempfile(tmpdir="~/tmp/")
     probes <- rowData(se_sig)$Probe_ID
     chrm <- sapply(strsplit(probes, "_"), function(x) x[1])
@@ -291,8 +292,8 @@ pack_cm_file <- function(se_sig, ref="mm10", tmpdir="~/tmp/") {
     write.table(data.frame(chrm=chrm, beg1=beg1-1, end=beg1+1,
         contrast=paste0(rowData(se_sig)$branch, ".as.", rowData(se_sig)$type)),
         file = temp_file, row.names = FALSE, col.names = FALSE, sep = "\t", quote=FALSE)
-    system(sprintf("sortbed %s | bedtools intersect -a ~/references/%s/annotation/cpg/cpg_nocontig.bed.gz -b - -sorted -loj | cut -f7 | yame pack -s - %s.cm", temp_file, ref, temp_file))
-    paste0(temp_file, ".cm")
+    system(sprintf("sortbed %s | bedtools intersect -a ~/references/%s/annotation/cpg/cpg_nocontig.bed.gz -b - -sorted -loj | bedtools groupby -g 1-3 -c 7 -o first | cut -f4 | yame pack -f s - %s.cx", temp_file, ref, temp_file))
+    paste0(temp_file, ".cx")
 }
 
 sig_summary <- function(cg_file, cm_file, sample=NULL, tmpdir="~/tmp/") {
